1b0ee3f1cb1b980365ee93edad60a19d706ffb9a
jscodelover <manisha.4918@gmail.com>
2020-02-18 15:55:10 +0530

    feat: video UI

--- script.js
+++ script.js
@@ -251,9 +251,6 @@ Vue.component('MovieCast', {
 						</div>`
 });
 
-// <MovieReviews v-if="tab"></MovieReviews>
-// <MovieMember v-else></MovieMember>
-
 Vue.component('MovieReview', {
 	template: `<div class="movie__reviews">
 							<div class="movie__reviews--post review">
@@ -273,28 +270,116 @@ Vue.component('MovieReview', {
 								</div>
 							</div>
 							<div class="movie__reviews--post review">
-							<span>
-							<img src="/images/frame.png" alt="avatar" />
-							</span>
-							<div class="review__content">
-								<h3 class="font-bold text-xl">A review by msbreviews</h3>
-								<p class="text-sm mb-6">
-									<span>Written by msbreviews on</span>
-									<span>November 26, 2019</span>
-								</p>
-								<div class="review__content--text">
-									<p class="text-base">The kingdom of Arendelle needs to be evacuated when the forces of nature threaten to destroy it. Elsa, Anna, Olaf and Kristoff set off to find some answers. But Elsa has been distracted. She has been hearing an unfamiliar voice calling out to her in a strange tune. Led by her, the group follows the melody to find themselves at the edge of an Enchanted Forest with untold mysteries and dangers... <a href="#" class="text-green-600">read the rest</a></p>
+								<span>
+									<img src="/images/frame.png" alt="avatar" />
+								</span>
+								<div class="review__content">
+									<h3 class="font-bold text-xl">A review by msbreviews</h3>
+									<p class="text-sm mb-6">
+										<span>Written by msbreviews on</span>
+										<span>November 26, 2019</span>
+									</p>
+									<div class="review__content--text">
+										<p class="text-base">The kingdom of Arendelle needs to be evacuated when the forces of nature threaten to destroy it. Elsa, Anna, Olaf and Kristoff set off to find some answers. But Elsa has been distracted. She has been hearing an unfamiliar voice calling out to her in a strange tune. Led by her, the group follows the melody to find themselves at the edge of an Enchanted Forest with untold mysteries and dangers... <a href="#" class="text-green-600">read the rest</a></p>
+									</div>
 								</div>
 							</div>
-						</div>
-
-
-
-
-
-
-
 						</div>`
+});
+Vue.component('MovieVideo', {
+	template: `<div class="movie__video">
+								<video-player class="vjs-custom-skin"
+									ref="videoPlayer"
+									:options="playerOptions"
+									:playsinline="true"
+									@play="onPlayerPlay($event)"
+									@pause="onPlayerPause($event)"
+									@ended="onPlayerEnded($event)"
+									@loadeddata="onPlayerLoadeddata($event)"
+									@waiting="onPlayerWaiting($event)"
+									@playing="onPlayerPlaying($event)"
+									@timeupdate="onPlayerTimeupdate($event)"
+									@canplay="onPlayerCanplay($event)"
+									@canplaythrough="onPlayerCanplaythrough($event)"
+									@ready="playerReadied"
+									@statechanged="playerStateChanged($event)">
+								</video-player>
+						</div>`,
+	data() {
+		return {
+			playerOptions: {
+				height: '360',
+				autoplay: false,
+				muted: false,
+				language: 'en',
+				playbackRates: [0.7, 1.0, 1.5, 2.0],
+				sources: [
+					{
+						type: 'video/mp4',
+						// mp4
+						src: 'http://vjs.zencdn.net/v/oceans.mp4'
+						// webm
+						// src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+					}
+				],
+				poster:
+					'https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg'
+			}
+		};
+	},
+	mounted() {
+		// console.log('this is current player instance object', this.player)
+		setTimeout(() => {
+			console.log('dynamic change options', this.player);
+			this.player.muted(false);
+		}, 2000);
+	},
+	computed: {
+		player() {
+			return this.$refs.videoPlayer.player;
+		}
+	},
+	methods: {
+		// listen event
+		onPlayerPlay(player) {
+			// console.log('player play!', player)
+		},
+		onPlayerPause(player) {
+			// console.log('player pause!', player)
+		},
+		onPlayerEnded(player) {
+			// console.log('player ended!', player)
+		},
+		onPlayerLoadeddata(player) {
+			// console.log('player Loadeddata!', player)
+		},
+		onPlayerWaiting(player) {
+			// console.log('player Waiting!', player)
+		},
+		onPlayerPlaying(player) {
+			// console.log('player Playing!', player)
+		},
+		onPlayerTimeupdate(player) {
+			// console.log('player Timeupdate!', player.currentTime())
+		},
+		onPlayerCanplay(player) {
+			// console.log('player Canplay!', player)
+		},
+		onPlayerCanplaythrough(player) {
+			// console.log('player Canplaythrough!', player)
+		},
+		// or listen state event
+		playerStateChanged(playerCurrentState) {
+			// console.log('player current update state', playerCurrentState)
+		},
+		// player is ready
+		playerReadied(player) {
+			// seek to 10s
+			console.log('example player 1 readied', player);
+			player.currentTime(10);
+			// console.log('example 01: the player is readied', player)
+		}
+	}
 });
 
 Vue.component('MovieContent', {
@@ -319,7 +404,7 @@ Vue.component('MovieContent', {
 								<div class="tab__content">
 									<MovieCast v-if="tab === 1"></MovieCast>
 									<MovieReview v-else-if="tab === 2"></MovieReview>
-									<div v-else>3</div>
+									<MovieVideo v-else>3</MovieVideo>
 								</div>
 							</div>
 						</div>
@@ -362,6 +447,7 @@ const router = new VueRouter({
 	routes
 });
 
+Vue.use(VueVideoPlayer);
 // Vue instance
 new Vue({
 	el: '#root',
