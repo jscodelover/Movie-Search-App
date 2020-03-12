d385a376467f565464e331bc149cfdf4c4d178c5
jscodelover <manisha.4918@gmail.com>
2020-03-12 17:12:20 +0530

    refactor: api integration for movie video

--- vue-movie-app/src/components/MovieVideo.vue
+++ vue-movie-app/src/components/MovieVideo.vue
@@ -1,40 +1,66 @@
 <template>
-	<vue-plyr>
-		<video
-			crossorigin
-			playsinline
-			poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
-			src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-		>
-			<source
-				size="576"
-				src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-				type="video/mp4"
-			/>
-			<!-- <source
-				size="720"
-				src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-720p.mp4"
-				type="video/mp4"
-			/>
-			<source
-				size="1080"
-				src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-1080p.mp4"
-				type="video/mp4"
-			/> -->
-			<!-- <a
-				download=""
-				href="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
-			>
-				Download
-			</a> -->
-		</video>
-	</vue-plyr>
+  <Fragment>
+    <div v-if="!videos.length">We don't have any videos</div>
+    <vue-plyr v-for="video in formattedVideos" :key="video.id">
+      <div data-plyr-provider="youtube" :data-plyr-embed-id="video.key"></div>
+    </vue-plyr>
+  </Fragment>
 </template>
 
 <script>
+import { Fragment } from "vue-fragment";
+import REQUEST from "@/utils/https.service";
+import CONFIG from "@/utils/config.js";
 export default {
-	name: 'MovieVideo'
+  name: "MovieVideo",
+  components: { Fragment },
+  data() {
+    return {
+      videos: []
+    };
+  },
+  created() {
+    this.getVideos();
+  },
+  computed: {
+    formattedVideos() {
+      return this.videos.length
+        ? this.videos.filter(video => video.site === "YouTube")
+        : [];
+    }
+  },
+  methods: {
+    async getVideos() {
+      try {
+        const { status, data } = await REQUEST({
+          method: "get",
+          url: `https://api.themoviedb.org/3/movie/${this.$route.params.id}/videos?api_key=${CONFIG.API_KEY}&language=en-US`
+        });
+        this.videos = status === 200 ? data.results : [];
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+@media (min-width: 700px) {
+  .plyr,
+  .plyr__video-embed iframe,
+  .plyr__poster {
+    height: 350px !important;
+  }
+  .plyr__poster {
+    width: auto !important;
+    background-position: initial !important;
+  }
+  .plyr:fullscreen {
+    .plyr__video-embed iframe,
+    .plyr__poster {
+      height: 100% !important;
+    }
+  }
+}
+</style>
