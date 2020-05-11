08fb8737887436d0d2da54472abe752805849282
jscodelover <manisha.4918@gmail.com>
2020-05-11 19:34:42 +0530

    feat: .env setup

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_KEY: "7a6be3a7d8314d666d431de2a459c893",
+  API_KEY: process.env.VUE_APP_API_KEY,
   IMAGE_SM: "https://image.tmdb.org/t/p/w185_and_h278_bestv2",
   IMAGE_LG: "https://image.tmdb.org/t/p/w1400_and_h450_face",
   IMAGE_MD: "https://image.tmdb.org/t/p/w300_and_h450_bestv2",
